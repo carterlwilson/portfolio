b49f5eb037f18349bb6246ed45622c3218604bb9
Carter <carterlwilson@gmail.com>
2020-04-16 14:28:12 -0700

    added emailjs

--- src/Contact.js
+++ src/Contact.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import './styling/Contact.css'
+import './styling/Contact.css';
+import emailjs from 'emailjs-com';
 
 class contact extends React.Component {
     render() {
         return(
             <div className="contact" id="contact">
                 <div className="contact-form-wrapper">
-                    <form>
+                    <form onSubmit={this.sendemail}>
                         <h2>Contact Me</h2>
-                        <label for="Name">name</label>
-                        <input type="text" name="Name"/>
-                        <label for="Email">email</label>
-                        <input type="email" name="Email"/>
-                        <label for="Message">message</label>
-                        <textarea name="Message"/>
-                        <input type="submit" value="Submit" id="submit-button"/>
+                        <label for="user_name">name</label>
+                        <input type="text" name="user_name"/>
+                        <label for="user_email">email</label>
+                        <input type="email" name="user_email"/>
+                        <label for="message">message</label>
+                        <textarea name="message"/>
+                        <input type="submit" value="Send" id="submit-button"/>
                     </form>
                 </div>
             </div>
         )
+    }
+
+    sendemail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm("gmail", "template_chBKfdgR", e.target, "user_N5e508TDs5fUIBFVJqXLc")
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
     }
 }
 
